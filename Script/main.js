4317dfe24f05a86f931a741348a17d09f20ed4df
SKRUZO <SKRUZO@gmail.com>
2017-09-01 18:23:17 -0700

    New Wallpapers
    Added more wallpapers

--- Script/main.js
+++ Script/main.js
@@ -54,16 +54,20 @@ function randomBG(){
 	var images=['Images/1.jpg',
 				'Images/2.jpg',
 				'Images/3.jpg',
-				'Images/4.png',
+				'Images/4.jpg',
 				'Images/5.jpg',
 				'Images/6.jpg',
 				'Images/7.jpg',
 				'Images/8.jpg',
 				'Images/9.jpg',
-				'Images/10.jpg',
+				'Images/10.png',
 				'Images/11.jpg',
 				'Images/12.jpg',
-				'Images/13.jpg'];
+				'Images/13.jpg',
+				'Images/14.jpg',
+				'Images/15.jpg',
+				'Images/16.jpg',
+				'Images/17.jpg'];
 	
 	$('.BG').css("background", "url("+ images[Math.floor(Math.random() * images.length)] + ") no-repeat center center fixed");
 	$('.BG').css("background-size", "cover");
