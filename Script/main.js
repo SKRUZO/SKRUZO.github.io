cb8b9f3b1c76eaa5cc70f6cf60d1ed426f0fbcc6
SKRUZO <SKRUZO@gmail.com>
2017-11-02 23:19:50 -0700

    Fall Wallpapers

--- Script/main.js
+++ Script/main.js
@@ -52,7 +52,20 @@ setInterval(updateTime, 250);
 //This could be a lot shorter if they all have the same extension but since this is a startpage so I value aesthetics more than code tidyness...
 //Syntax: 'Images/1.jpg',
 function randomBG(){
-	var images=['Images/1.jpg'];
+	var images=['Images/1.jpg',
+				'Images/2.jpg',
+				'Images/3.jpg',
+				'Images/4.jpg',
+				'Images/5.jpg',
+				'Images/6.jpg',
+				'Images/7.jpg',
+				'Images/8.jpg',
+				'Images/9.png',
+				'Images/10.jpg',
+				'Images/11.jpg',
+				'Images/12.jpg',
+				'Images/13.jpg',
+				'Images/14.jpg'];
 	
 	$('.BG').css("background", "url("+ images[Math.floor(Math.random() * images.length)] + ") no-repeat center center fixed");
 	$('.BG').css("background-size", "cover");
