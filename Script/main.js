30b339845955c2e0bccc582ceb2412dc19f80758
SKRUZO <SKRUZO@gmail.com>
2018-01-22 22:55:37 -0800

    New Wallpapers

--- Script/main.js
+++ Script/main.js
@@ -52,32 +52,17 @@ setInterval(updateTime, 250);
 //This could be a lot shorter if they all have the same extension but since this is a startpage so I value aesthetics more than code tidyness...
 //Syntax: 'Images/1.jpg',
 function randomBG(){
-	var images=['Images/1.png',
-				'Images/2.png',
-				'Images/3.png',
-				'Images/4.png',
-				'Images/5.png',
-				'Images/6.png',
-				'Images/7.png',
-				'Images/8.png',
-				'Images/9.png',
-				'Images/10.png',
-				'Images/11.png',
-				'Images/12.png',
-				'Images/13.png',
-				'Images/14.png',
-				'Images/15.png',
-				'Images/16.png',
-				'Images/17.png',
-				'Images/18.png',
-				'Images/19.png',
-				'Images/20.png',
-				'Images/21.png',
-				'Images/22.png',
-				'Images/23.png',
-				'Images/24.png',
-				'Images/25.png',
-				'Images/26.png'];
+	var images=['Images/1.jpg',
+				'Images/2.jpg',
+				'Images/3.jpg',
+				'Images/4.jpg',
+				'Images/5.jpg',
+				'Images/6.jpg',
+				'Images/7.jpg',
+				'Images/8.jpg',
+				'Images/9.jpg',
+				'Images/10.jpg',
+				'Images/11.jpg'];
 	
 	$('.BG').css("background", "url("+ images[Math.floor(Math.random() * images.length)] + ") no-repeat center center fixed");
 	$('.BG').css("background-size", "cover");
